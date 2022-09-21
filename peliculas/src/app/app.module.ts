deda8cc88eab5eb95c68f88a647942038053b9ae
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-09-21 01:43:43 -0500

    instalando material

--- peliculas/src/app/app.module.ts
+++ peliculas/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
+import { MaterialModule } from './material/material.module';
+import { MenuComponent } from './menu/menu.component';
+import { ListadoPeliculasComponent } from './listado-peliculas/listado-peliculas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndiceGenerosComponent
+    IndiceGenerosComponent,
+    MenuComponent,
+    ListadoPeliculasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
